491cb96599d99d81796064e50cde145307a21834
kai-zen <razipurali@gmail.com>
2022-02-09 09:11:13 +0330

    Created MyNavBar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyLayout from './components/MyLayout';
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MyAppBar.jsx
@@ -0,0 +1,55 @@
+import { Favorite, Menu as MenuIcon, ShoppingCart } from '@mui/icons-material';
+import {
+  AppBar,
+  Badge,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+
+// const MyAppBar = ({ handleDrawerToggle }) => {
+const MyAppBar = () => {
+  return (
+    <AppBar
+      sx={{
+        width: { sm: 'calc(100% - 260px)' },
+        ml: { sm: '260px' },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 1, display: { xs: 'block', sm: 'none' } }}
+          // onClick={handleDrawerToggle}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, fontWeight: 'bold' }}
+        >
+          Food Palace 2
+        </Typography>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <Favorite />
+          </Badge>
+        </IconButton>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MyAppBar;
