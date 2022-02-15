ef25fcc5567fbf6b10fd36c2764b135ae990a510
kai-zen <razipurali@gmail.com>
2022-02-15 11:57:55 +0330

    Background and structure of search tab

---
+++ src/components/tabs/serach-tab/SearchTabPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import SearchInput from './SearchInput';
+// import SearchSingleCard from './SingleCard';
+
+const SearchTabPanel = () => {
+  return (
+    <>
+      //<SearchInput />
+      //<SearchSingleCard />
+    </>
+  );
+};
+
+export default SearchTabPanel;
